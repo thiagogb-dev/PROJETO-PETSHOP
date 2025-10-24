d0da203df92f54515a54ce32727c83f44a593e8a
thiagogb-dev <thiagoghost59@gmail.com>
2025-10-24 18:49:02 -0300

    codigo-base

--- petshop.py
+++ petshop.py
@@ -1,63 +1,64 @@
-# Listas  para armazenamento de dados
+# Listas para armazenamento de dados
 usuarios = []
 produtos = []
 servicos = []
 
 # Menu para cadastro ou Login de usuario
 while True:
-    print('---MENU---')
+    print('-------------BEM VINDO AO PETSHOP-------------')
+    print('---CADASTRE-SE OU FAÇA LOGIN PARA CONTINUAR---')
     print('1- Cadastro de usuário')
     print('2- Fazer login')
+    print('3- Sair')
 
-    op = int(input('Qual sua opção: '))
+    op = int(input('Qual sua opção?: '))
 
-    # Criando identificação para usuario
+    # Cadastro de usuário
     if op == 1:
         print('------------------')
-        print('Esse cadastro é para um ADMIN ou Para um Cliente?')
-        identificacao = input('Identificação: ').lower()
+        identificacao = input('Esse cadastro é para um ADMIN ou CLIENTE? ').lower()
         
-        # Armazenando os dados do ADMIN
-        if identificacao == 'admin':
-            print('------------------')
-            nome = input('Insira seu nome de usuario: ').lower()
-            senha = input('Insira sua senha: ').lower()
-            print('Cadastro Concluido')
-            print('------------------')
-
-            adm = [nome, senha]
-            usuarios.append(adm)
-        
-        # Armazenando os dados do Cliente
-        elif identificacao == 'cliente':
-            print('------------------')
-            nome = input('Insira seu nome de usuario: ').lower()
-            senha = input('Insira sua senha: ').lower()
-            print('Cadastro Concluido')
-            print('------------------')
+        while identificacao != 'admin' or identificacao == 'cliente':
+            print('Identificação inválida, O sistema só funciona com ADMINS ou CLIENTES!!.')
+            identificacao = input('Esse cadastro é para um ADMIN ou CLIENTE? ').lower()
             
-            cliente = [nome, senha]
-            usuarios.append(cliente)
+            if identificacao == 'admin' or identificacao == 'cliente':
+                nome = input('Insira seu nome de usuario: ').lower()
+                senha = input('Insira sua senha: ').lower()
+                usuarios.append([identificacao, nome, senha])
+                print('------------------')
+                print('Cadastro Concluido!')
+                print('------------------')
+            break
+            
+
+    # Login
+    elif op == 2:
+        print('------------------')
+        nome = input('Insira seu nome de usuario: ').lower()
+        senha = input('Insira sua senha: ').lower()
+
+        achou = False
+        for usuario in usuarios:
+            if usuario[1] == nome and usuario[2] == senha:
+                print('------------------')
+                print('Acesso concedido')
+                print('------------------')
+                if identificacao == 'admin':
+                    print('------MENU DE ADM-----')
+                else:
+                    print('------MENU DE CLIENTE-----')
+                break
 
         else:
-             print('Identificação inválida')
-
-    if op == 2:
-        print('------------------')
-        nome = input('Insira seu nome de usuario: ')
-        senha = input('Insira sua senha: ')
-        
-        if (nome and senha) not in usuarios:
-            print('------------------')
+            print('------------------------------------------')
             print('Usuário não cadastrado ou Login Inválido')
-            print('------------------')
+            print('------------------------------------------')
 
-        elif (nome and senha) in usuarios[0]:    
-            print('------------------')
-            print('Acesso concedido')
-            print('------MENU DE ADM-----')
+    elif op == 3:
+        print('Saindo...')
+        break
 
-        elif (nome and senha) in usuarios[1]:    
-            print('------------------')
-            print('Acesso concedido')
-            print('------MENU DE CLIENTE-----')
+    else:
+        print('------------------')
+        print('Opção inválida!')
